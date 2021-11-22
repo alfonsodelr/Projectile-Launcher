2117340f7627e9ec0dff6415ca35ea57a27e3b74
alfonsodelr <delrosarioluis22@gmail.com>
2021-11-21 23:48:10 -0800

    Added Title Screen

--- Projectile Launch.py
+++ Projectile Launch.py
@@ -1,7 +1,6 @@
-
-
 import pygame, sys
 from PIL import Image
+from pygame import mouse
 
 from pygame.constants import K_SPACE, KEYDOWN, MOUSEBUTTONDOWN, MOUSEBUTTONUP, QUIT
 
@@ -13,12 +12,11 @@ def main():
     clock = pygame.time.Clock()
     pygame.init()
     WINDOW_SIZE = (600,800)
-    pygame.display.set_caption('MY GAME')
+    pygame.display.set_caption('Projectile Launcher')
 
     font = pygame.font.Font('freesansbold.ttf', 20)
     score = 0
     text = font.render('Score: ' + str(score), True, green, blue)
-
 
     display = pygame.display.set_mode(WINDOW_SIZE)
     player_image = pygame.image.load('testing.png')
@@ -27,19 +25,25 @@ def main():
 
     starting_location = [280, 700]
     ball_location = starting_location
+
     enemy_location = [100, 180]
-    enemy_location_2 = [460, 180]
+    enemy_location_2 = [460, 120]
     ball_dx = 0
     ball_dy = 0
     e1_life = True
     e2_life = True
     rect_color = (255,0,0)
+    enemy1_dx = 30
+    enemy2_dx = 30
 
+    begin = 0
     movement = False
 
     while True:
         
+        #End Screen
         if score == 2:
+            begin = 2
             display.fill((146,244,255))
             font = pygame.font.Font('freesansbold.ttf', 40)
             text = font.render('CONGRATULATIONS', True, green, blue)
@@ -51,13 +55,24 @@ def main():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-        else:
+        
+        #Game Loop
+        if begin == 1:
+
             display.fill((146,244,255))
             pygame.draw.rect(display, rect_color, pygame.Rect(0,1,600, 10))
             display.blit(player_image, ball_location)
 
             text = font.render('Score: ' + str(score), True, green, blue)
             display.blit(text, (10,20))
+
+            enemy_location[0] += enemy1_dx * 0.10
+            enemy_location_2[0] -= enemy2_dx * 0.10
+
+            if enemy_location[0] > WINDOW_SIZE[0] - 50 or enemy_location[0] < 0:
+                enemy1_dx *= -1
+            if enemy_location_2[0] > WINDOW_SIZE[0] - 50 or enemy_location_2[0] < 0:
+                enemy2_dx *= -1
 
             if e1_life:
                 display.blit(enemy_image, enemy_location)
@@ -118,9 +133,34 @@ def main():
 
             pygame.display.update()
             clock.tick(60)
-        
 
-    
+        #begin screen
+        elif begin == 0:
+            display.fill((146,244,255))
+            font = pygame.font.Font('freesansbold.ttf', 40)
+            text_1 = font.render('WELCOME', True, blue, green)
+            text_2 = font.render('TO', True, blue, green)
+            text_3 = font.render('MY GAME', True, blue, green)
+            display.blit(text_1, (100,100))
+            display.blit(text_2, (100,140))
+            display.blit(text_3, (100,180))
+
+            font = pygame.font.Font('freesansbold.ttf', 30)
+            play = font.render('Press Space To Start', True, green, blue)
+            display.blit(play, (160, 450))
+
+            pygame.display.update()
+            clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_SPACE:
+                        begin = True
+
+        
 main()
 
 
